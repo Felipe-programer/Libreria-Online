be6bca7af0b74afc8e36ba665f887d50cf7e5f32
Juan Felipe Jimenez Valencia <felipejimenez616@hotmail.com>
2021-10-19 19:49:52 -0500

    Se agrega funcionalidad y suscripcion

--- js/function.js
+++ js/function.js
@@ -1,17 +1,9 @@
-/* funcionalidad de pagos */
+﻿/* funcionalidad de pagos */
+document.getElementById("libro1").value=0;
+document.getElementById("libro2").value=0;
+document.getElementById("libro3").value=0;
+document.getElementById("libro4").value=0;
 
-/* inputs */
-
-var libro1 = document.getElementById("libro1").value;
-libro1=parseInt(libro1);
-var libro2 = document.getElementById("libro2").value;
-libro2=parseInt(libro2);
-var libro3 = document.getElementById("libro3").value;
-libro3=parseInt(libro3);
-var libro4 = document.getElementById("libro4").value;
-libro4=parseInt(libro4);
-/*boton de pago */
-var btnPagar =document.getElementById("pagar");
 
 /* impresión de articulos */
 /* var articulos = document.getElementById("printArt"); */
@@ -30,39 +22,80 @@ var u4 = document.getElementById("unidad4");
 /* total a pagar */
 
 var pago = document.getElementById("precio");
+/*boton de pago */
+var btnPagar =document.getElementById("pagar");
+var Carrito =document.getElementById("btnCarrito");
+var Carrito2 =document.getElementById("btnCarrito2")
 
-btnPagar.addEventListener("click",pagar);
 
-
+/* btnPagar.addEventListener("click",pagar); */
 var total=0;
-function pagar (){
+Carrito.addEventListener("click",cargue);
+Carrito2.addEventListener("click",cargue);
 
-    /* articulos.innerHTML=articulos.innerHTML + "Articulos del Carrito" */
+var validacion = false;
+
+function cargue (){
+
+
+    /* inputs */
+
+        var libro1 = document.getElementById("libro1").value;
+        libro1=parseInt(libro1);
+        var libro2 = document.getElementById("libro2").value;
+        libro2=parseInt(libro2);
+        var libro3 = document.getElementById("libro3").value;
+        libro3=parseInt(libro3);
+        var libro4 = document.getElementById("libro4").value;
+        libro4=parseInt(libro4);
+    /* limpiar total */
+    pago.innerHTML=pago.innerHTML+"";
 
     if(libro1>0){
         p1.innerHTML=p1.innerHTML+"Alexander Cane : Programación con Python";
         u1.innerHTML=u1.innerHTML+libro1;
-        total+=(libro1*15);
+        total+=(libro1*35000);
     }
     if(libro2>0){
         p2.innerHTML=p2.innerHTML+"Alfredo Moreno Muñoz : Python para Principiantes";
         u2.innerHTML=u2.innerHTML+libro2;
-        total+=(libro2*25);
+        total+=(libro2*66000);
         
     }
     if(libro3>0){
         p3.innerHTML=p3.innerHTML+"Alexander Cane : Programación para principiantes";
         u3.innerHTML=u3.innerHTML+libro3;
-        total+=(libro3+25);
+        total+=(libro3+98000);
         
     }
     if(libro4>0){
         p4.innerHTML=p4.innerHTML+"Rafael Goméz Blanes : El libro negro del programador";
         u4.innerHTML=u4.innerHTML+libro4;
-        total+=(libro4*25);
+        total+=(libro4*45000);
         
     }
-    pago.innerHTML=pago.innerHTML+"$ "+total;
-
-    alert(total);
+    if(total>0){
+        pago.innerHTML=pago.innerHTML+"$ "+total;
+        document.getElementById("libro1").value=0;
+        document.getElementById("libro2").value=0;
+        document.getElementById("libro3").value=0;
+        document.getElementById("libro4").value=0;
+        validacion = true;
+    }else{
+        alert("No tiene articulos en el cariito, vaya al catalogo");
+    }
+    return total;
 }
+
+        btnPagar.addEventListener("click",transaccion)   
+
+    function transaccion(){
+        if(validacion){
+                alert("LA TRANSACCIÓN HA SIDO EXITOSA!");
+    
+        } else{
+            alert("EL CARRITO SE ENCUENTRA VACÍO");
+        }   
+    }
+
+    
